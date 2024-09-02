c66d4981f5086139c745d898b8fc47714d92451a
blackspherefollower <darktech@black-sphere.co.uk>
2024-09-02 10:00:14 +0100

    feat: cowgirl cone and amorelie-joy support

---
+++ buttplug/src/server/device/protocol/amorelie_joy.rs
@@ -0,0 +1,43 @@
+// Buttplug Rust Source Code File - See https://buttplug.io for more info.
+//
+// Copyright 2016-2024 Nonpolynomial Labs LLC. All rights reserved.
+//
+// Licensed under the BSD 3-Clause license. See LICENSE file in the project root
+// for full license information.
+
+use crate::{
+  core::{errors::ButtplugDeviceError, message::Endpoint},
+  server::device::{
+    hardware::{HardwareCommand, HardwareWriteCmd},
+    protocol::{generic_protocol_setup, ProtocolHandler},
+  },
+};
+
+generic_protocol_setup!(AmorelieJoy, "amorelie-joy");
+
+#[derive(Default)]
+pub struct AmorelieJoy {}
+
+impl ProtocolHandler for AmorelieJoy {
+  fn keepalive_strategy(&self) -> super::ProtocolKeepaliveStrategy {
+    super::ProtocolKeepaliveStrategy::RepeatLastPacketStrategy
+  }
+
+  fn handle_scalar_vibrate_cmd(
+    &self,
+    index: u32,
+    scalar: u32,
+  ) -> Result<Vec<HardwareCommand>, ButtplugDeviceError> {
+    Ok(vec![HardwareWriteCmd::new(
+      Endpoint::Tx,
+      [
+        0x01,        // static header
+        0x01,        // pattern (1 = steady),
+        scalar as u8,// speed 0-100
+      ]
+      .to_vec(),
+      false,
+    )
+    .into()])
+  }
+}
