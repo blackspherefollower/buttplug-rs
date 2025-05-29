e5b7a9cb307bb0eb205638d69e6d791b3f56af58
blackspherefollower <darktech@black-sphere.co.uk>
2025-05-29 09:19:23 +0100

    feat: Adding more iToys device support (specifically oscillators)

--- buttplug/src/server/device/protocol/itoys.rs
+++ buttplug/src/server/device/protocol/itoys.rs
@@ -35,4 +35,24 @@ impl ProtocolHandler for IToys {
     )
     .into()])
   }
+
+  fn handle_scalar_oscillate_cmd(
+    &self,
+    _index: u32,
+    scalar: u32,
+  ) -> Result<Vec<HardwareCommand>, ButtplugDeviceError> {
+    Ok(vec![HardwareWriteCmd::new(
+      Endpoint::Tx,
+      vec![
+        0xa0,
+        0x06,
+        if scalar == 0 { 0x00 } else { 0x01 },
+        0x00,
+        if scalar == 0 { 0x00 } else { 0x01 },
+        scalar as u8,
+      ],
+      false,
+    )
+    .into()])
+  }
 }
