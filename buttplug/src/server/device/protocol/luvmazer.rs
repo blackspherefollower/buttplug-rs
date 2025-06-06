0fcbdf7ff110f0fa040395571b895bb301f45154
blackspherefollower <darktech@black-sphere.co.uk>
2025-06-06 15:57:58 +0100

    fixup luvmazer

--- buttplug/src/server/device/protocol/luvmazer.rs
+++ buttplug/src/server/device/protocol/luvmazer.rs
@@ -30,19 +30,34 @@ use tokio::time::sleep;
 
 generic_protocol_initializer_setup!(Luvmazer, "luvmazer");
 
-async fn delayed_rotate_handler(device: Arc<Hardware>, scalar: u8) {
-  sleep(Duration::from_millis(25)).await;
+async fn delayed_handler(device: Arc<Hardware>, cmd: u8, mode: u8, idx:u8, scalar: u8) {
+  sleep(Duration::from_millis(50)).await;
   let res = device
     .write_value(&HardwareWriteCmd::new(
       Endpoint::Tx,
-      vec![0xa0, 0x0f, 0x00, 0x00, 0x64, scalar as u8],
+      vec![0xa0, cmd, mode, idx, 0x64, scalar as u8],
       false,
     ))
     .await;
   if res.is_err() {
-    error!("Delayed Luvmazer Rotate command error: {:?}", res.err());
+    error!("Delayed Luvmazer command error: {:?}", res.err());
   }
 }
+
+async fn delayed_handler_long(device: Arc<Hardware>, cmd: u8, mode: u8, idx:u8, bits:u8, scalar: u8) {
+  sleep(Duration::from_millis(100)).await;
+  let res = device
+      .write_value(&HardwareWriteCmd::new(
+        Endpoint::Tx,
+        vec![0xa0, cmd, mode, idx, 0x64, scalar as u8],
+        false,
+      ))
+      .await;
+  if res.is_err() {
+    error!("Delayed Luvmazer command error: {:?}", res.err());
+  }
+}
+
 #[derive(Default)]
 pub struct LuvmazerInitializer {}
 
@@ -82,12 +97,17 @@ impl ProtocolHandler for Luvmazer {
     } else {
       None
     };
-
+    let cmd3 = if commands.len() > 2 {
+      commands[2]
+    } else {
+      None
+    };
+    
     if let Some(cmd) = cmd2 {
       if cmd.0 == ActuatorType::Rotate {
         if cmd1.is_some() {
           let dev = self.device.clone();
-          async_manager::spawn(async move { delayed_rotate_handler(dev, cmd.1 as u8).await });
+          async_manager::spawn(async move { delayed_handler(dev, 0x0f,0x00, 0x00, cmd.1 as u8).await });
         } else {
           return Ok(vec![HardwareWriteCmd::new(
             Endpoint::Tx,
@@ -96,26 +116,52 @@ impl ProtocolHandler for Luvmazer {
           )
           .into()]);
         }
-      }
-    }
-
-    if let Some(cmd) = cmd2 {
-      if cmd.0 == ActuatorType::Oscillate {
+      } else if cmd.0 == ActuatorType::Oscillate {
         if cmd1.is_some() {
           let dev = self.device.clone();
-          async_manager::spawn(async move { delayed_rotate_handler(dev, cmd.1 as u8).await });
+          async_manager::spawn(async move { delayed_handler(dev, 0x06, 0x01, 0x00, cmd.1 as u8).await });
         } else {
           return Ok(vec![HardwareWriteCmd::new(
             Endpoint::Tx,
             vec![0xa0, 0x06, 0x01, 0x00, 0x64, cmd.1 as u8],
             false,
           )
-          .into()]);
+              .into()]);
+        }
+      } else if cmd.0 == ActuatorType::Vibrate {
+        if cmd1.is_some() {
+          let dev = self.device.clone();
+          async_manager::spawn(async move { delayed_handler(dev, 0x01, 0x00, 0x01, cmd.1 as u8).await });
+        } else {
+          return Ok(vec![HardwareWriteCmd::new(
+            Endpoint::Tx,
+            vec![0xa0, 0x01, 0x00, 0x01, 0x64, cmd.1 as u8],
+            false,
+          )
+              .into()]);
         }
       }
     }
+
+
+    if let Some(cmd) = cmd3 {
+      if cmd.0 == ActuatorType::Constrict {
+          if cmd1.is_some() {
+            let dev = self.device.clone();
+            async_manager::spawn(async move { delayed_handler_long(dev, 0x01, 0x00, 0x01, if cmd.1 == 0 {0x00} else {0x64}, cmd.1 as u8).await });
+          } else {
+            return Ok(vec![HardwareWriteCmd::new(
+              Endpoint::Tx,
+              vec![0xa0, 0x0d, 0x00, 0x00, if cmd.1 == 0 {0x00} else {0x64}, cmd.1 as u8],
+              false,
+            )
+                .into()]);
+          }
+      }
+    }
+      
     /*
-    vec![0xa0, 0x0d, 0x00, 0x00, 0x64, 0x00], # inflate
+    vec![0xa0, 0x0d, 0x00, 0x00, 0x64, 0xff], # inflate
     vec![0xa0, 0x0d, 0x00, 0x00, 0x00, 0x00], # deflate
      */
 
