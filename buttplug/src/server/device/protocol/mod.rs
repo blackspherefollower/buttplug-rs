08bf7e12c894d86c802378ef9e28ab00d26a3c50
blackspherefollower <darktech@black-sphere.co.uk>
2025-06-13 17:26:14 +0100

    feat: Add support for Sagan vibe

--- buttplug/src/server/device/protocol/mod.rs
+++ buttplug/src/server/device/protocol/mod.rs
@@ -100,6 +100,7 @@ pub mod pink_punch;
 pub mod prettylove;
 pub mod raw_protocol;
 pub mod realov;
+pub mod sagan;
 pub mod sakuraneko;
 pub mod satisfyer;
 pub mod sensee;
@@ -524,6 +525,10 @@ pub fn get_default_protocol_map() -> HashMap<String, Arc<dyn ProtocolIdentifierF
     raw_protocol::setup::RawProtocolIdentifierFactory::default(),
   );
   add_to_protocol_map(&mut map, realov::setup::RealovIdentifierFactory::default());
+  add_to_protocol_map(
+    &mut map,
+    sagan::setup::SaganIdentifierFactory::default(),
+  );
   add_to_protocol_map(
     &mut map,
     sakuraneko::setup::SakuranekoIdentifierFactory::default(),
